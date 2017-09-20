7948fbf26a9c361a2ca290b83107a082c9f4e142
Marcos Felipe Eipper <marcos@eipper.com.br>
2017-09-20 15:26:32 -0300

    commit

---
+++ vue-bulma/src/components/Input.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="field">
+    <label class="label">{{label}}</label>
+    <div class="control">
+      <input class="input" :type="type" :placeholder="placeholder">
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      label: String,
+      placeholder: String,
+      type: {
+        type: String,
+        default: 'text'
+      }
+    }
+  }
+</script>
--- vue-bulma/src/views/Hello.vue
+++ vue-bulma/src/views/Hello.vue
@@ -1,13 +1,24 @@
 <template>
-  <bulma-hero :titulo="titulo" :subtitulo="subtitulo"></bulma-hero>
+  <div>
+    <bulma-hero :titulo="titulo" :subtitulo="subtitulo"></bulma-hero>
+      <form>
+        <bulma-input label="Nome" placeholder="Nome da pessoa"></bulma-input>
+        <bulma-input label="Idade"
+                     placeholder="Idade da pessoa"
+                     type="number"></bulma-input>
+      </form>
+  </div>
 </template>
 
 <script>
 import BulmaHero from '../components/Hero'
+import BulmaInput from '../components/Input'
+
 export default {
   name: 'hello',
   components: {
-    BulmaHero
+    BulmaHero,
+    BulmaInput
   },
   data() {
     return {
